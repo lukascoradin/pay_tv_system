0f8d873f00dbc42f8a927c04050024793f3bdbcb
Lucas Coradin <lukascoradin@hotmail.com>
2025-07-23 10:52:31 -0300

    fix indentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ plan_two = Plan.create!(name: "Plano Total", price: 200.00)
 service_one = AdditionalService.create!(name: "HBO", price: 30.00)
 service_two = AdditionalService.create!(name: "Telecine", price: 40.00)
 
-package_one = Package.create!(name: "Pacote Filmes", plan: plan_two, additional_services: [service_one, service_two], price: 250.00)
-package_two = Package.create!(name: "Pacote HBO", plan: plan_one, additional_services: [service_one])
+package_one = Package.create!(name: "Pacote Filmes", plan: plan_two, additional_services: [ service_one, service_two ], price: 250.00)
+package_two = Package.create!(name: "Pacote HBO", plan: plan_one, additional_services: [ service_one ])
 
 subscription_one = Subscription.create!(client: client_one, plan: plan_one)
-subscription_two = Subscription.create!(client: client_two, plan: plan_two, additional_services: [service_one, service_two])
+subscription_two = Subscription.create!(client: client_two, plan: plan_two, additional_services: [ service_one, service_two ])
 subscription_one = Subscription.create!(client: client_one, package: package_one)
